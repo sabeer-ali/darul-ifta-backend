40c35d98e2b952aef37ece52d95b8242a6a55613
Sabeer ali <sabeer@sectorqube.com>
2022-02-16 00:26:31 +0530

    test gitignore

--- config/datastores.js
+++ config/datastores.js
@@ -47,5 +47,6 @@ module.exports.datastores = {
      ***************************************************************************/
     adapter: "sails-mysql",
     url: "mysql://root:@localhost:3307/fatwas",
+    //test
   },
 };
